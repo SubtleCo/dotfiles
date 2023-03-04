711384c7abd4ddfb94c8a27c67b23e560d42b729
Alex Martin <alex.martin@getbuilt.com>
2023-03-04 09:58:25 -0600

    Do you have any history of mental illness in your family? I have an uncle who does yoga.

---
+++ .config/nvim/lua/user/vimtest.lua
@@ -0,0 +1,6 @@
+vim.g["test#strategy"] = "neovim"
+-- vim.g["test#neovim#start_normal"] = 1
+-- vim.g["test#neovim#term_position"] = "vert botright split"
+vim.g["test#python#runner"] = "pytest"
+vim.g["test#javascript#runner"] = "jest"
+-- vim.g["test#javascript#jest#file_pattern"] = "\\v(__tests__/.*|(spec|test|unit))\\.(js|jsx|coffee|ts|tsx)$"
