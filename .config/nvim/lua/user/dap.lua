00e09469b25fc87132cd42905b80e0e814fe5fc8
Alex Martin <alex.martin@getbuilt.com>
2023-03-17 08:22:10 -0500

    Shorts over six inches are capri pants, shorts under six inches are European.

--- .config/nvim/lua/user/dap.lua
+++ .config/nvim/lua/user/dap.lua
@@ -13,6 +13,12 @@ if not dap_ui_status_ok then
   return
 end
 
+
+
+------------
+-- PYTHON --
+------------
+
 local dap_python_status_ok, dap_python = pcall(require, "dap-python")
 if not dap_python_status_ok then
   return
@@ -23,8 +29,43 @@ dap_python.test_runner = "pytest"
 -- Install an environment with debugpy and point to the binary
 dap_python.setup("~/.local/share/virtualenvs/debugpy-1pX4fgu6/bin/python")
 -- rather than dump a bunch of configs here, use the .vscode/launch.json file in the repo to provide launch options
-vscode.load_launchjs()
 
+-----------------------------
+-- JavaScript & TypeScript --
+-----------------------------
+
+require("dap-vscode-js").setup({
+  -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+  -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+  -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+  adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+  -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+  -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+  -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+})
+
+for _, language in ipairs({ "typescript", "javascript" }) do
+  require("dap").configurations[language] = {
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+  },
+  {
+    type = "pwa-node",
+    request = "attach",
+    name = "Attach",
+    processId = require'dap.utils'.pick_process,
+    cwd = "${workspaceFolder}",
+  }
+}
+end
+
+
+vscode.load_launchjs()
+-- vscode.load_launchjs(nil, { node = { 'typescript', 'javascript' }})
 
 dapui.setup {
   expand_lines = true,
